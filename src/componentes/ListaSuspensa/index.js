e972d69e7b3928e4f5d1ee59e7aec154d78194a0
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-04-06 13:36:25 -0300

    Criando card dos colaboradores

--- src/componentes/ListaSuspensa/index.js
+++ src/componentes/ListaSuspensa/index.js
@@ -5,6 +5,7 @@ const ListaSuspensa = (props) => {
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required}>
+                <option value="none" selected disabled hidden>selecione uma opção</option>
                 {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
         </div>
