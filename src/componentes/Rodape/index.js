60bc5c5aae270c9ddd6198393a3e4c73cf0fea81
Daniel Caresma Silva <daniel.caresma@gmail.com>
2023-04-07 00:17:55 -0300

    Criando o rodapé

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -3,21 +3,35 @@ import './Rodape.css'
 const Rodape = () => {
     return (
         <footer className='rodape'>
-            <div className='redes-sociais'>
-                <img src='../../public/imagens/fb.png' alt='Facebook' />
-                <img src='../../public/imagens/tw.png' alt='Twitter' />
-                <img src='../../public/imagens/ig.png' alt='Instagram' />
-            </div>
+            <section> 
+                <ul>
+                    <li>
+                        <a href='facebook.com' target='_blank'>
+                            <img src='/imagens/fb.png' alt='Facebook' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href='twitter.com' target='_blank'>
+                        <img src='/imagens/tw.png' alt='Twitter' />
+                        </a>
+                    </li>
+                    <li>
+                        <a href='instagram.com' target='_blank'>
+                            <img src='/imagens/ig.png' alt='Instagram'/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+              
+            <section>
+                <img src='/imagens/logo.png' alt='Instagram' />
+            </section>
                 
-            <div className='logo'>
-                <img src='%PUBLIC_URL%/imagens/logo.png' alt='Instagram' />
-            </div>
-                
-            <div className='texto'>
+            <section>
                 <p>
                     Desenvolvido por Alura.
                 </p>
-            </div>
+            </section>
         </footer>
     )
 }
